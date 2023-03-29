a217ee487c8152327d89019a54d3f46620ab151d
hariharan2109 <108514077+hariharan2109@users.noreply.github.com>
2023-03-29 15:24:20 +0530

    Add files via upload

---
+++ src/Components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../Components/about.css"
+export default function About() {
+  return (
+   <section id='about'>
+    <div className='aboutdiv'>
+    <div className='about-div'>
+       <div className='top'>
+    </div>
+   <div className='bottom'>
+    <div>
+    <h1>Taste and See Coffee Shop </h1>
+    <p>and Gallery offers a unique coffee house environment unlike any other in Macon. We are not only a place to drop in and get your morning cup of coffee (although you are more than welcome to do that), we are a place where you can sit down and enjoy that tailor-made cup of coffee. If you need to work, we have a seating area created specifically for you</p>
+   
+    </div>
+    </div> 
+   </div>
+   </section>
+  )
+}
